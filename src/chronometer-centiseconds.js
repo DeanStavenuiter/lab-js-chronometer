c0b4494e7dead7bf0f2922db28e18dbec15ad030
DeanStavenuiter <info@deanstavenuiter.nl>
2023-01-17 16:11:39 +0100

    cleaned up

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -45,5 +45,3 @@ class Chronometer {
     return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds()) + "." + this.computeTwoDigitNumber(this.getCentiseconds());
   }
 }
-
-console.log(120 % 60);
